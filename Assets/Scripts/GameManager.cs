7dd192ec7239b63d07b65ddcef0789a49dfc085a
Karina <karina.popp@nyu.edu>
2022-10-03 14:40:06 -0400

    Part 5: Setting up the game scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //game object for all buttons
+    public GameObject choiceOne;
+    public GameObject choiceTwo;
+    public GameObject nextButton;
+
+    //text component that is showing the dialogue
+    public TMP_Text dialogueBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //turn off the choice buttons
+        choiceOne.SetActive(false);
+        choiceTwo.SetActive(false);
+        SetDialogueText();
+    }
+
+    void SetDialogueText() {
+        //set the dialogue component to show the line we're on
+        dialogueBox.text = "Hey, it's some dialogue!";
+    }
+
+    void SetupChoices() {
+        //turn off the next button and turn on the choice buttons
+        nextButton.SetActive(false);
+        choiceOne.SetActive(true);
+        choiceTwo.SetActive(true);
+    }
+
+}
